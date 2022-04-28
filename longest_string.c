ee0539b997d7ed56fb182af77c7d58292bb9a395
Vishnu-Gaddime <vishnu.gaddime@cctech.co.in>
2022-04-28 09:57:39 +0530

    removed if condition

--- longest_string.c
+++ longest_string.c
@@ -2,21 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
-{
-    if (argc == 1)
-    {
-        puts("");
-    }
- 
+int main( int argc, char *argv[] )
+{ 
     int longestWordIndex = 1;
-    for (int i = 1; i < argc; i++)
+    for ( int i = 2; i < argc; i++ )
     {
-        if (strlen(argv[longestWordIndex]) < strlen(argv[i]))
+        if ( strlen(argv[longestWordIndex]) < strlen(argv[i]) )
         {
             longestWordIndex = i;
+            puts(argv[longestWordIndex]);
         }
     }
-    puts(argv[longestWordIndex]);
+    puts("");
     return EXIT_SUCCESS;
 }
